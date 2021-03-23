0f1c060c46085bfe4f19e9806df46a3c8e6ba2c0
Orlz <52678852+Orlz@users.noreply.github.com>
2021-03-23 07:53:56 +0100

    Create create_vision_venv.sh

---
+++ Assignments/assignment04/create_vision_venv.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+VENVNAME=cv101 
+
+python3 -m venv $VENVNAME
+source $VENVNAME/bin/activate
+pip install --upgrade pip
+
+# problems when installing from requirements.txt
+pip install ipython
+pip install jupyter
+pip install matplotlib
+pip install opencv-python
+pip install networkx
+pip install sklearn
+pip install seaborn
+
+python -m ipykernel install --user --name=$VENVNAME
+
+test -f requirements.txt && pip install requirements.txt
+
+deactivate
+echo "build $VENVNAME"
